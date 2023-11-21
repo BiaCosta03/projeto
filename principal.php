54ba082b173204405d0297abe1596fcf6421155f
kaliny <kaliny.g@escolar.ifrn.edu>
2023-11-21 10:37:53 -0300

    teste

--- principal.php
+++ principal.php
@@ -3,13 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet'  type='text/css'  href='principal.css'>
     <title>SIGAP</title>
 </head>
 <body>
-    <h1>Seja Bem-Vindo(a)</h1>
-        <p><h2>Esse é um projeto desenvolvido por alunos do IFRN-Campus Canguaretama,</br>
+    <div class="secao">
+        <div class="container">
+            <div class="informacoes">
+            <h1>Seja Bem-Vindo(a)</h1>
+            <p><h2>Esse é um projeto desenvolvido por alunos do IFRN-Campus Canguaretama,</br>
             tem como objetivo ajudar no desenvolvimento do sistema de gestão escolar da escola municipal
             Professora Nazaré de Andrade Duarte(ND).</h2>
-        </p>
+            </p>
+            </div>
+            <div class="login">
+            <form action="login.php" method="POST">
+                <h1>Login</h1>
+
+                <p>
+                <label>Matrícula</label>
+                <input type="Number" name="matricula">
+                </p>
+
+                <p>
+                <label>Senha</label>
+                <input type="Number" name="senha">
+                </p>
+                
+                <div class="formlogin">
+                    <div class="botaoform">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
