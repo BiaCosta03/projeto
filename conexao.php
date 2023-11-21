323f02ee65aa64b726f319b8deddfd557cf9cc42
kaliny <kaliny.g@escolar.ifrn.edu>
2023-11-21 16:02:51 -0300

    banco

--- conexao.php
+++ conexao.php
@@ -12,10 +12,10 @@ $mat_professor_turmas = "";
 $turmas = "";
 $turno = "";
 
-$sistemaacademico = new sistemaacademico($hostname, $bancodedados, $professor,$adm, $aluno, $boletim, $categoria, 
+$mysqli = new mysqli($hostname, $bancodedados, $professor,$adm, $aluno, $boletim, $categoria, 
 $disciplina, $mat_aluno_turmas, $mat_professor_turmas, $turmas, $turno);
-if($sistemaacademico->connect_errno){
-    echo"Falha ao conectar:(".$sistemaacademico->connect_errno. ")" . $sistemaacademico->connect_errno;
+if($mysqli->connect_errno){
+    echo"Falha ao conectar:(".$mysqli->connect_errno. ")" . $mysqli->connect_errno;
 }
 else
 echo"conectando ao banco de dados";
