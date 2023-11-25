5ac703d8b81a5c008023dc05ee5fbe8c81fafc59
BiaCosta03 <beatriz.oliveira1@escolar.ifrn.edu.br>
2023-11-24 22:42:36 -0300

    organizando

---
+++ banco/conexao.php
@@ -0,0 +1,9 @@
+<?php
+$hostname = "localhost";
+$bancodedados = "sistemaacademico";
+$usuario = "root";
+$senha = "";
+
+    $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+    
+?>
