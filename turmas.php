e352c9121c5757acf340d449251331d35aa7af78
Kalinyg <kaliny.g@escolar.ifrn.edu.br>
2023-11-30 15:18:59 -0300

    atualizar

---
+++ turmas.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Turmas</title>
+    <style>
+        body {
+            background-image: url(img/icon.png);
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: rgba(12, 46, 12, 0.4);
+            font-weight: bold;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+<h2>Sistema de Turmas</h2>
+
+<table>
+    <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Turno</th>
+        <th>Vagas</th>
+        <th>Alunos Matriculados</th>
+    </tr>
+    <tr>
+        <td>101</td>
+        <td>Português</td>
+        <td>Manhã</td>
+        <td>20</td>
+        <td>15</td>
+    </tr>
+    <tr>
+        <td>102</td>
+        <td>Matemática</td>
+        <td>Manhã</td>
+        <td>25</td>
+        <td>20</td>
+    </tr>
+    <tr>
+        <td>103</td>
+        <td>História</td>
+        <td>Tarde</td>
+        <td>20</td>
+        <td>18</td>
+    </tr>
+    <tr>
+        <td>104</td>
+        <td>Geografia</td>
+        <td>Tarde</td>
+        <td>25</td>
+        <td>22</td>
+    </tr>
+</table>
+
+</body>
+</html>
