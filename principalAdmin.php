e6062d74acc56d0ec23c3ed5ff46dd8328c2f080
kaliny.g@escolar.ifrn.edu.br <kaliny.g@escolar.ifrn.edu.br>
2023-12-14 17:26:54 -0300

    pg atualizada

--- principalAdmin.php
+++ principalAdmin.php
@@ -7,6 +7,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+      body{
+        background-color: #91DF76;
+      }
+      li {
+        display: inline;
+    }
+      a{
+        display: block;
+        padding: 70px 0;
+        background-color: #98FB98;
+        margin-left: 25px;
+        width: 100%;
+      }
+    </style>
    
 </head>
 <body>
