70495a4b68bb9eed3e2d18955d7d7fac925dcf37
BiaCosta03 <112516850+BiaCosta03@users.noreply.github.com>
2023-12-19 10:18:45 -0300

    Update editarTurmas.php

--- editarTurmas.php
+++ editarTurmas.php
@@ -6,23 +6,15 @@
 <div class="row">
     <div class="col-12">
         <h1>Editar Turma</h1>
-        <form action="editarTurmas.php" method="POST">
-            <input type="hidden" name="id" value="<?php echo $_GET["idTurmas"] ?>">
+        <form action="?page=salvarTurmas" method="POST">
+            <input type="hidden" name="id" value="<?php print $row->idTurmas; ?>">
             <div class="form-group">
                 <label for="nome">Nome</label>
-                <input value="<?php echo $turmas->nome ?>" name="nome" required type="text" id="nome" class="form-control" placeholder="Nome">
+                <input value="<?php print $row->nome ?>" name="nome" required type="text" id="nome" class="form-control" placeholder="Nome">
             </div>
             <div class="form-group">
                 <label for="turnos">Turno</label>
-                <input value="<?php echo $turmas->nome ?>" name="vagas" required type="int" id="turnos" class="form-control" placeholder="turnos">
-            </div>
-            <div class="form-group">
-                <label for="vagas">Vagas</label>
-                <input value="<?php echo $turmas->nome ?>" name="vagas" required type="int" id="vagas" class="form-control" placeholder="vagas">
-            </div>
-            <div class="form-group">
-                <label for="alunosMatriculados">Alunos matriculados</label>
-                <input value="<?php echo $turmas->nome ?>" name="alunosMatriculados" required type="int" id="alunosMatriculados" class="form-control" placeholder="alunosMatriculados">
+                <input value="<?php print $row->nome ?>" name="vagas" required type="int" id="turnos" class="form-control" placeholder="turnos">
             </div>
             <div class="form-group">
                 <button class="btn btn-success" type="submit">Editar</button>
