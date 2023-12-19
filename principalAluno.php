635944fc60b49b6b7436e7779e769b1a8cadba1c
BiaCosta03 <112516850+BiaCosta03@users.noreply.github.com>
2023-12-19 10:29:27 -0300

    Update principalAluno.php

--- principalAluno.php
+++ principalAluno.php
@@ -7,7 +7,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-   
+    <style>
+      body{
+        background-color: #91DF76;
+      }
+      li {
+        display: inline;
+    }
+      a{
+        display: block;
+        padding: 70px 0;
+        background-color: #98FB98;
+        margin-left: 25px;
+        width: 100%;
+      }
+      header{
+        width: 1220px;
+        max-width: 200px;
+        max-height:150px;
+        top: 0px;
+        right: 0px;
+        left: 0px;
+        margin: 0px auto;
+      }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -25,15 +48,21 @@
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="page=principalAluno">Home</a>
+          <a class="nav-link active" aria-current="page" href="principalAluno.php">Home</a>
         </li>
           <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link active" href="?page=boletim" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Boletim
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="?page=boletim">boletim</a></li>
-          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active" href="?page=infoUsuario" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Imformações Pessoais
+          </a>
+        </li>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="sair.php">Sair</a>
         </li>
     </nav>
     <div class="container mt-3">
@@ -48,6 +77,11 @@
               case 'boletim':
                 include('boletim.php');
                 break;
+
+                case 'infoUsuario':
+                  include('infoUsuario.php');
+                  break;
+
               default:
               print"";
             }
