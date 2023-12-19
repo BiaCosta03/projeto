56f7acb856ce08ddec328262af2ce740fab87da0
BiaCosta03 <112516850+BiaCosta03@users.noreply.github.com>
2023-12-19 10:17:04 -0300

    Update editarAlunos.php

--- editarAlunos.php
+++ editarAlunos.php
@@ -1,32 +1,33 @@
 <?php
-	$sql = "SELECT * FROM aluno WHERE idAluno=".$_REQUEST['idAluno'];
-	$res = $mysqli->query($sql);
-	$row = $res->fetch_object();
+   $sql = "SELECT * FROM aluno WHERE idAluno=".$_REQUEST['idAluno'];
+   $res = $mysqli->query($sql);
+   $row = $res->fetch_object();
 ?>
 <div class="row">
     <div class="col-12">
         <h1>Editar estudante</h1>
-        <form action="editarAlunos.php" method="POST">
-            <input type="hidden" name="idAluno" value="<?php echo $_GET["idAluno"] ?>">
+        <form action="?page=salvarAlunos" method="POST">
+            <input type="hidden" name="acao" value="editar">
+            <input type="hidden" name="idAluno" value="<?php print $row->idAluno; ?>">
             <div class="form-group">
                 <label for="nome">Nome</label>
-                <input value="<?php echo $Aluno->nome ?>" name="nome" required type="text" id="nome" class="form-control" placeholder="Nome">
+                <input value="<?php print $row->nome ?>" name="nome" required type="text" id="nome" class="form-control" placeholder="Nome">
             </div>
             <div class="form-group">
                 <label for="grupo">Senha</label>
-                <input value="<?php echo $Aluno->senha ?>" name="senha" required type="text" id="senha" class="form-control" placeholder="senha">
+                <input value="<?php print $row->senha ?>" name="senha" required type="text" id="senha" class="form-control" placeholder="senha">
             </div>
 			<div class="form-group">
                 <label for="cpf">cpf</label>
-                <input value="<?php echo $Aluno->cpf ?>" name="cpf" required type="varchar" id="cpf" class="form-control" placeholder="cpf">
+                <input value="<?php print $row->cpf ?>" name="cpf" required type="varchar" id="cpf" class="form-control" placeholder="cpf">
             </div>
 			<div class="form-group">
                 <label for="telefone">Telefone</label>
-                <input value="<?php echo $Aluno->grupo ?>" name="telefone" required type="tel" id="telefone" class="form-control" placeholder="Telefone">
+                <input value="<?php print $row->telefone ?>" name="telefone" required type="tel" id="telefone" class="form-control" placeholder="Telefone">
             </div>
 			<div class="form-group">
-                <label for="date">Data de Nascimento</label>
-                <input value="<?php echo $Aluno->grupo ?>" name="date" required type="date" id="data_nascimento" class="form-control" placeholder="date">
+                <label for="data_nascimento">Data de Nascimento</label>
+                <input value="<?php print $row->data_nascimento ?>" name="data_nascimento" required type="date" id="data_nascimento" class="form-control" placeholder="date">
             </div>
             <div class="form-group">
                 <button class="btn btn-success" type="submit">Editar</button>
