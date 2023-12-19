55d48901a7e15d683306a4137948b1c5916b09e3
BiaCosta03 <112516850+BiaCosta03@users.noreply.github.com>
2023-12-19 10:19:27 -0300

    Update editarTurnos.php

--- editarTurnos.php
+++ editarTurnos.php
@@ -6,11 +6,11 @@
 <div class="row">
     <div class="col-12">
         <h1>Editar Turma</h1>
-        <form action="editarTurnos.php" method="POST">
+        <form action="?page=salvarTurnos" method="POST">
             <input type="hidden" name="id" value="<?php echo $_GET["idTurnos"] ?>">
             <div class="form-group">
                 <label for="nome">Nome</label>
-                <input value="<?php echo $turnos->nome ?>" name="nome" required type="text" id="nome" class="form-control" placeholder="Nome">
+                <input value="<?php print $row->nome ?>" name="nome" required type="text" id="nome" class="form-control" placeholder="Nome">
             </div>
             <div class="form-group">
                 <button class="btn btn-success" type="submit">Editar</button>
